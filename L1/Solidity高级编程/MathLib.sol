9ff9616b05df40c718ab35feeae18cfd916f599f
chenxiao-god <cx50996@gmail.com>
2025-03-02 20:33:13 +0800

    L1

--- L1/Solidity高级编程/MathLib.sol
+++ L1/Solidity高级编程/MathLib.sol
@@ -1,19 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 library MathLib{
-    function add(uint a, uint b) public pure returns (uint){
+    function add(uint a, uint b) external pure returns (uint){
         return a + b;
     }
-    function sub(uint a, uint b) public pure returns (uint){
+    function sub(uint a, uint b) external pure returns (uint){
         return a - b;
     }
-    function mul(uint a, uint b) public pure returns (uint){
+    function mul(uint a, uint b) external pure returns (uint){
         return a * b;
     }
-    function div(uint a, uint b) public pure returns (uint){
+    function div(uint a, uint b) external pure returns (uint){
         return a / b;
     }
-    function mod(uint a, uint b) public pure returns (uint){
+    function mod(uint a, uint b) external pure returns (uint){
         return a % b;
     }
 }
